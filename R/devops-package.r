95c16fd7d02435f5bbabc1db795efb9d9eddf1ea
Janko Thyson <janko.thyson@rappster.de>
2015-10-30 15:12:30 +0100

    initial commit

---
+++ R/devops-package.r
@@ -0,0 +1,37 @@
+#' @details Details
+#' @section Classes and constructors:
+#'
+#' \itemize{
+#'    \item{\code{\link[dataconr]{TODO}}: }{
+#'
+#'      TODO
+#'    }
+#' }
+#'
+#' @section Main functions/methods:
+#'
+#'  \itemize{
+#'    \item{\code{\link[dataconr]{TODO}}: }{
+#'
+#'      TODO
+#'    }
+#' }
+#'
+#' @section Disclaimer:
+#' \strong{This package is work-in-progress and is thus likely to change quite
+#' substantially until it reaches a more mature state.}
+#'
+#' @section Aknowledgements:
+#' This package was inspired by the following resources:
+#'
+#' \itemize{
+#'    \item{Package \code{devtools}: } {
+#'
+#'      \url{https://github.com/hadley/devtools/}
+#'    }
+#' }
+#'
+#' @template authors
+#' @template references
+#' @seealso See \code{vignette("introduction", package = "devops")}.
+"_PACKAGE"
---
+++ README.md
@@ -0,0 +1,9 @@
+---
+title: "devops"
+author: "Janko Thyson"
+date: "30. Oktober 2015"
+output: html_document
+---
+
+[![Travis-CI Build Status](https://travis-ci.org/<USERNAME>/<REPO>.svg?branch=master)](https://travis-ci.org/<USERNAME>/<REPO>)
+[![Coverage Status](https://img.shields.io/codecov/c/github/<USERNAME>/<REPO>/master.svg)](https://codecov.io/github/<USERNAME>/<REPO>?branch=master)
